d75f74893db6aa78fa6c586aa7e7972781f427cb
Christian Suncin <SunciNx10@gmail.com>
2021-10-23 21:21:52 -0600

    Update
    Some UI changes added

--- animaciones.js
+++ animaciones.js
@@ -22,29 +22,49 @@ const projectText = document.querySelector('.copy');
 const projectText2 = document.querySelector('.copy-2');
 const btnProject = document.getElementById('btn-project');
 const skillPack = document.querySelectorAll('.skill_hover');
-const imgSkills = document.getElementById('img-skills');
+const pSkills = document.querySelector('.p_skills');
+const btnAllProjects = document.getElementById('btn-all-projects');
+const btnLanding = document.getElementById('btn-landing');
+const btnNode = document.getElementById('btn-node');
 
 //Esta es la función para el hover en skills-sct
 for(let i=0;i<skillPack.length;i++){
     skillPack[i].addEventListener('mouseover', ()=>{
         switch (i){
             case 0:
-                imgSkills.src = 'img/html.png';
+                pSkills.textContent = 'Aquí inicia todo, tengo más 10 meses trabajando con HTML y sé sobre los nuevos estandares de HTML5, semantica, buenas prácticas y SEO.';
+                pSkills.style.color = 'orange';
                 break;
             case 1:
-                imgSkills.src = 'img/css.png';
+                pSkills.textContent = 'Tengo experiencia con la adaptación a diferentes resoluciones de pantalla (Responsive design) en lo cuál tengo más de 10 meses de experiencia, incluyendo HTML emails y los nuevos estandares de CSS3.';
+                pSkills.style.color = 'blue';
                 break;
             case 2:
-                imgSkills.src = 'img/javascript.png';
+                pSkills.textContent = 'El lenguaje de la web, tengo más de 6 meses de experiencia tanto al utilizarlo para crear animaciones, como para complementar junto con los media queries de CSS al realizar el responsive design hasta lo que implica el front end al momento de consumir APIs, conectar con el back end para el intercambio de información, manipulación del DOM, promesas, this, POO, protocolos HTTP, eventos, await/async, JSON, XML y los nuevos estandares de ESC6.';
+                pSkills.style.color = 'yellow';
                 break;
             case 3:
-                imgSkills.src = 'img/nodejs.png';
+                pSkills.textContent = 'Continuando con JavaScript, ahora del lado del servidor, tengo más de 6 meses de experiencia trabajando el backend con NodeJS, utilizando NPM y populares dependencias como Express, Nodemon, CORS, Mongoose, Body-Parse. Amplio conocimiento en el uso de export/import, rutas, modelos, creación de APIs, MVC, comunicación con base de datos (MongoDB) y el front-end.';
+                pSkills.style.color = 'greenyellow';
                 break;
             case 4:
-                imgSkills.src = 'img/mongoDB.png';
+                pSkills.textContent = 'Tengo 1 mes trabajando con ASP.Net, MVC, POO, Entities, comunicación con base de datos SQL para realizar consultas, métodos GET y POST.';
+                pSkills.style.color = 'purple';
+                break;
+            case 5:
+                pSkills.textContent = 'Experiencia de 2 meses con Git para control de versiones, he utilizado Shell, GitKraken, BitBucket y GitHub.';
+                pSkills.style.color = '#f34f29';
+                break;
+            case 6:
+                pSkills.textContent = 'Complementando el stack de JavaScript, tengo más de 6 meses de experiencia en la creación de CRUDs. Comprendo el sistema de esquemas, colecciones, documentos y subdocumentos de MongoDB, los métodos POST, PUT, GET y DELETE.';
+                pSkills.style.color = 'green';
+                break;
+            case 7:
+                pSkills.textContent = 'Experiencia de 2 meses en el lenguaje de base de datos relacional SQL, he utilizado Oracle SQL y SQL Server para consultas, filtros, envío de datos y actualizaciones de los mismos, métodos JOIN, SET, UPDATE, DELETE.';
+                pSkills.style.color = 'lightblue';
                 break;
             default:
-                imgSkills.src = 'img/skills.png';
+                // imgSkills.src = 'img/skills.png';
                 break;
         }
     });
@@ -79,8 +99,8 @@ window.addEventListener('scroll', ()=>{
     if(xSkills<detenerScrollSkills){
         skills.style.transform = `translateX(${xSkills}px)`;
     }
-    console.log('Scroll a cambiar: '+ xSkills);
-    console.log('Detener scroll: '+ detenerScrollSkills);
+    // console.log('Scroll a cambiar: '+ xSkills);
+    // console.log('Detener scroll: '+ detenerScrollSkills);
 
     //1094
     if(scrollActual<scrollMostrarProyectos){
@@ -102,17 +122,31 @@ window.addEventListener('scroll', ()=>{
     }
 });
 
-// LightBox
-// imgView.forEach(element => {
-//     element.addEventListener('click', ()=>{
-//         lightBox.classList.toggle('move');
-//         if(element[0]){
-//             console.log('elemento 0');
-//         }else{
-//             console.log('other one');
-//         }
-//     });
-// });
+// Filtrar proyectos
+btnAllProjects.addEventListener('click', ()=>{
+    for(let i=0;i<imgView.length;i++){
+        imgView[i].classList.remove('ocultar');
+    }
+});
+btnLanding.addEventListener('click', ()=>{
+    for(let i=0;i<imgView.length;i++){
+        if(imgView[i].classList.contains('landing')){
+            imgView[i].classList.remove('ocultar');
+        }else{
+            imgView[i].classList.add('ocultar');
+        }
+    }
+});
+btnNode.addEventListener('click', ()=>{
+    for(let i=0;i<imgView.length;i++){
+        if(imgView[i].classList.contains('node')){
+            imgView[i].classList.remove('ocultar');
+        }else{
+            imgView[i].classList.add('ocultar');
+        }
+    }
+});
+
 for(let i=0;i<imgView.length;i++){
     imgView[i].addEventListener('click', ()=>{
         lightBox.classList.toggle('move');
@@ -128,14 +162,19 @@ for(let i=0;i<imgView.length;i++){
                 btnProject.setAttribute('href', 'https://suncin18.github.io/SoftLoop/');
                 break;
             case 2:
+                projectText.innerText = 'Tecnologías utilizadas: HTML, CSS, JS';
+                projectText2.innerText = 'Landing page para un restaurante ficticio con su respectivo menú de comidas, responsive y con un menú de navegación fijo.';
+                btnProject.setAttribute('href', 'https://suncin18.github.io/restaurant/');
+                break;
+            case 3:
                 projectText.innerText = 'Tecnologías utilizadas: HTML, CSS, JS, NodeJS, MongoDB y Git';
                 projectText2.innerText = 'FullStack app con HTML, CSS y JS en el front-end y NodeJS en el back-end, el back-end está en Heroku y el front-end en GitHub, la base de datos está almacenada por medio de AWS con la función que ofrece MongoDB Atlas. La aplicación tiene un log in básico, recibe información del usuario para generar una factura la cual se envía por un responsive HTML email, usando gmail y además se muestra en la sección "historial". Si no desea registrarse (la aplicación le permitirá hacerlo) puede usar estos credenciales: USUARIO: tdevelopment4@gmail.com CONTRASEÑA: Sun#66';
                 btnProject.setAttribute('href', 'https://suncin18.github.io/FacturacionD/');
                 break;
-            case 3:
-                projectText.innerText = 'Tecnologías utilizadas: HTML, CSS, JS';
-                projectText2.innerText = 'Landing page para un restaurante ficticio con su respectivo menú de comidas, responsive y con un menú de navegación fijo.';
-                btnProject.setAttribute('href', 'https://suncin18.github.io/restaurant/');
+            case 4:
+                projectText.innerText = 'Tecnologías utilizadas: HTML, CSS, JS, NodeJS, MongoDB y Git';
+                projectText2.innerText = 'FullStack JavaScrip app para agendar citas en un gimnasio, se genera un código aleatoreo y se almacena en una base de datos, tiene un CRUD por lo que el usuario también puede cancelar su cita y el administrador puede ver la lista de todas las citas agendadas además de poder filtrarlas por fecha o código. Se implementan peticiones con la biblioteca AXIOS, se utiliza mongoose, express, nodemon, routes y otras bibliotecas en el back end.';
+                btnProject.setAttribute('href', 'https://suncin18.github.io/gym-ticket/');
                 break;
             default:
                 break;
